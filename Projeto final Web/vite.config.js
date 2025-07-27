d424b0fd589caff6072be270f294a7be5dd9a251
Felipe Muniz <85654430+felaipes@users.noreply.github.com>
2025-07-26 21:47:21 -0300

    Update vite.config.js

--- Projeto final Web/vite.config.js
+++ Projeto final Web/vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/brasilapi': {
+        target: 'https://brasilapi.com.br',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/brasilapi/, ''),
+      }
+    }
+  }
 })
